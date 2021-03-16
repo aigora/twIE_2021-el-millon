2f94d62276ced09b5c4402c19fafbe58d813df25
SolracZ21 <67156182+SolracZ21@users.noreply.github.com>
2021-03-16 10:00:41 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+char letra1 = 160; //á
+char letra2 = 130; //é
+char letra3 = 161; //í
+char letra4 = 162; //ó
+char letra5 = 163; //ú
+int main()
+{
+    printf("Hola, bienvenidos a ElMill%cn, una competici%cn que se basa en las capacidades intelectuales de cada individuo", letra4, letra4);
+    return 0;
+}
